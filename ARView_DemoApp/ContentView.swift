c59652d8e29ca626a6f4814cfdc97fd6f6badff5
Vigneshkumar <vigneshzx@gmail.com>
2023-10-09 19:01:04 +0530

    scencekit added

--- ARView_DemoApp/ContentView.swift
+++ ARView_DemoApp/ContentView.swift
@@ -6,14 +6,46 @@
 //
 
 import SwiftUI
+import SceneKit
 
 struct ContentView: View {
     var body: some View {
-        CustomViewRepresentable()
-            .ignoresSafeArea()
+        VStack {
+            Text("Hello scenekit in swiftUI")
+            SceneKitView()
+                .frame(width: 300, height: 300)
+                .background(Color.purple)
+        }
     }
 }
 
 #Preview {
     ContentView()
+}
+
+
+struct SceneKitView: UIViewRepresentable {
+    func makeUIView(context: Context) -> SCNView {
+        // Create a new SceneKit view
+        let sceneView = SCNView()
+        
+        // Create a new scene
+        let scene = SCNScene()
+        
+        // Create a box geometry
+        let boxGeometry = SCNBox(width: 1.0, height: 1.0, length: 1.0, chamferRadius: 0.0)
+        let boxNode = SCNNode(geometry: boxGeometry)
+        
+        // Add the box node to the scene
+        scene.rootNode.addChildNode(boxNode)
+        
+        // Set the scene to the SceneKit view
+        sceneView.scene = scene
+        
+        return sceneView
+    }
+    
+    func updateUIView(_ uiView: SCNView, context: Context) {
+        // Update the SceneKit view if needed
+    }
 }
